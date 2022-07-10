3b08d5417836b089db1f5ef4ea57fddbeefc226f
Agreewithu <3180101070@zju.edu.cn>
2022-07-10 21:24:26 +0800

    version/2022/07/10/lab

---
+++ vue-cli/hello-world/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const Home = () => import('../views/HomeView.vue')
+const About = () => import('../views/AboutView.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component:About
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
